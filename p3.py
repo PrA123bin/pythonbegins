443fb0bcb1e61844d95fc9cc46f897405aa0eba0
PrA123bin <pudasainikedarraj@gmail.com>
2024-11-04 11:20:18 +0400

    initial add

---
+++ p3.py
@@ -0,0 +1,7 @@
+a = float(input("enter first : "))
+b = float(input("enter second : "))
+
+
+avg = (a + b)/2
+
+print("avg=", (a + b)/2)
---
+++ p4.py
@@ -0,0 +1,6 @@
+a = int(input("enter first : "))
+b = int(input("enter second : "))
+
+
+
+print(a>=b)
